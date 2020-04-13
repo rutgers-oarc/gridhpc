b02bc71f2f253a17d4ccc689a6cedafba3ad22a9
Bill Abbott <babbott@rutgers.edu>
2020-04-13 02:02:28 -0400

    added cpu and gpu job check

--- jobstat.sh
+++ jobstat.sh
@@ -7,11 +7,12 @@ do clear
   echo
   echo $cluster:
   echo
+  echo gpu:
+  /home/someguy/gridhpc/pestat -u someguy -p gpu -C|awk '{print $1"  "$3"  "$5"  "$6"  "$8"  "$9}' |tail -n +6|tail -90
+  echo
   echo cpu:
   /home/someguy/gridhpc/pestat -u someguy -p main -C|awk '{print $1"  "$3"  "$5"  "$6"  "$8"  "$9}' |tail -n +6|tail -90
   echo
-  echo gpu:
-  /home/someguy/gridhpc/pestat -u someguy -p gpu -C|awk '{print $1"  "$3"  "$5"  "$6"  "$8"  "$9}' |tail -n +6|tail -90
   sleep 5
 done
 
